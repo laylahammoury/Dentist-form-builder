e8307510cc663ab4457c8cef12af05c6f3677656
laylahammoury <laylahammoury@gmail.com>
2019-09-05 00:50:24 +0300

    creating objects in the form Builder class

--- FormBuilder.php
+++ FormBuilder.php
@@ -6,16 +6,17 @@ class FormBuilder implements FormBuilderInterface
     var $stringTest =  "temp";
     public function buildPerson()
     {
-        $person = new Person();
+        $person = new Person("layla" , "Hammoury" , "Female" , "laylahammoury@gmail.com" , "www.blabla.com"
+        ,"022222222" , "121212" , "123123123" , "5" );
     }
 
     public function buildEducation()
     {
-       $certificate = new Education();
+       $certificate = new Education("Bachelor" ,"Computer Science ", "Palestine Polytechnic University","2020");
     }
 
     public function buildAddress()
     {
-       $address = new Address();
+       $address = new Address("Palestine", "West Bank ","Hebron" , "41","456456");
     }
 }
